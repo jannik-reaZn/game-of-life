634f5fa4c5e65768f2d72163c3c28ad55915fa44
jannik-reaZn <jannik.harm@gmail.com>
2026-05-13 19:31:38 +0000

    Stop tracking target directory

--- target/debug/deps/game_of_life-75728b24caa14095.d
+++
@@ -1,5 +0,0 @@
-/home/jannik/Development/game-of-life/target/debug/deps/game_of_life-75728b24caa14095.d: src/main.rs
-
-/home/jannik/Development/game-of-life/target/debug/deps/libgame_of_life-75728b24caa14095.rmeta: src/main.rs
-
-src/main.rs:
--- target/debug/deps/game_of_life-983d0b8a3efba3e2.d
+++
@@ -1,5 +0,0 @@
-/home/jannik/Development/game-of-life/target/debug/deps/game_of_life-983d0b8a3efba3e2.d: src/main.rs
-
-/home/jannik/Development/game-of-life/target/debug/deps/libgame_of_life-983d0b8a3efba3e2.rmeta: src/main.rs
-
-src/main.rs:
