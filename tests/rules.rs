772406085440916ee5e239495f80cf8f692f5bcf
jannik-reaZn <jannik.harm@gmail.com>
2026-05-14 11:17:43 +0000

    Added test for handling living cells

--- tests/rules.rs
+++ tests/rules.rs
@@ -1,6 +1,7 @@
-use game_of_life::domain::rules::{born, die, handle_living_cell, survive};
 use game_of_life::domain::cell::Cell;
+use game_of_life::domain::rules::{born, die, handle_living_cell, survive};
 use game_of_life::domain::state::State;
+use rstest::*;
 
 #[test]
 fn test_survive_rule() {
@@ -23,15 +24,38 @@ fn test_die_rule() {
     assert_eq!(die(&cell), true);
 }
 
-
-#[test]
-fn test_handle_living_cell() {
+#[rstest]
+#[case(2)]
+#[case(3)]
+fn test_handle_living_cell(#[case] living_cells: usize) {
     // GIVEN
-    let mut cell = Cell::new(State::Live, Cell::create_neighbours(2));
+    let mut cell = Cell::new(State::Live, Cell::create_neighbours(living_cells));
 
     // WHEN
     handle_living_cell(&mut cell);
 
     // THEN
     assert_eq!(cell.state(), State::Live);
+}
+
+#[rstest]
+#[case(0)]
+#[case(1)]
+#[case[4]]
+#[case[8]]
+fn test_handle_living_cell_bad(#[case] living_cells: usize) {
+    // GIVEN
+    let mut cell = Cell::new(State::Live, Cell::create_neighbours(living_cells));
+
+    // WHEN
+    handle_living_cell(&mut cell);
+
+    // THEN
+    assert_eq!(
+        cell.neighbours()
+            .iter()
+            .filter(|neighbour| neighbour.state() == State::Live)
+            .count(),
+        living_cells
+    );
 }
