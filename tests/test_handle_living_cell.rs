71869ae77208e44db919570f28ffc8cbc423db1e
jannik-reaZn <jannik.harm@gmail.com>
2026-05-14 11:40:33 +0000

    Added edge case test

--- tests/test_handle_living_cell.rs
+++ tests/test_handle_living_cell.rs
@@ -1,5 +1,5 @@
 use game_of_life::domain::cell::Cell;
-use game_of_life::domain::rules::{born, die, handle_living_cell, survive};
+use game_of_life::domain::rules::handle_living_cell;
 use game_of_life::domain::state::State;
 use rstest::*;
 
@@ -20,8 +20,8 @@ fn test_handle_living_cell(#[case] living_cells: usize) {
 #[rstest]
 #[case(0)]
 #[case(1)]
-#[case[4]]
-#[case[8]]
+#[case(4)]
+#[case(8)]
 fn test_handle_living_cell_bad(#[case] living_cells: usize) {
     // GIVEN
     let mut cell = Cell::new(State::Live, Cell::create_neighbours(living_cells));
@@ -37,4 +37,16 @@ fn test_handle_living_cell_bad(#[case] living_cells: usize) {
             .count(),
         living_cells
     );
+}
+
+#[test]
+fn test_handle_living_cell_when_cell_is_dead_does_not_change_state() {
+    // GIVEN
+    let mut cell = Cell::new(State::Dead, Cell::create_neighbours(1));
+
+    // WHEN
+    handle_living_cell(&mut cell);
+
+    // THEN
+    assert_eq!(cell.state(), State::Dead);
 }
