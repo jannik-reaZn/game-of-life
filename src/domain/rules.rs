afb5643a4f2c251039dfdca9c4c938b40a17a239
jannik-reaZn <jannik.harm@gmail.com>
2026-05-14 12:46:30 +0000

    Refactored born rule

--- src/domain/rules.rs
+++ src/domain/rules.rs
@@ -24,18 +24,23 @@ pub fn survive(cell: &Cell) -> bool {
 
 /// A dead cell with exactly three live neighbours becoomes a live cell.
 pub fn born(cell: &Cell) -> bool {
-    let neighbours = cell.neighbours();
-    let mut nof_life_neighbours = 0 as i8;
-    for neighbour in neighbours {
-        match neighbour.state() {
-            State::Live => nof_life_neighbours += 1,
-            State::Dead => continue,
+    match cell.state() {
+        State::Live => false,
+        State::Dead => {
+            let neighbours = cell.neighbours();
+            let mut nof_life_neighbours = 0 as i8;
+            for neighbour in neighbours {
+                match neighbour.state() {
+                    State::Live => nof_life_neighbours += 1,
+                    State::Dead => continue,
+                }
+            }
+            if nof_life_neighbours == 3 {
+                return true;
+            }
+            false
         }
     }
-    if nof_life_neighbours == 3 {
-        return true;
-    }
-    false
 }
 
 /// A live cell with fewer than two or more than three live neighbours die
--- tests/test_born_rule.rs
+++ tests/test_born_rule.rs
@@ -1,9 +1,42 @@
 use game_of_life::domain::cell::Cell;
 use game_of_life::domain::rules::born;
 use game_of_life::domain::state::State;
+use rstest::rstest;
 
 #[test]
-fn test_born_rule() {
+fn test_dead_cell_is_born() {
+    // GIVEN
     let cell = Cell::new(State::Dead, Cell::create_neighbours(3));
-    assert_eq!(born(&cell), true);
+
+    // WHEN
+    let shoud_be_born = born(&cell);
+
+    // THEN
+    assert_eq!(shoud_be_born, true);
+}
+
+#[rstest]
+#[case(2)]
+#[case(4)]
+fn test_dead_cell_stays_dead(#[case] living_cells: usize) {
+    // GIVEN
+    let cell = Cell::new(State::Dead, Cell::create_neighbours(living_cells));
+
+    // WHEN
+    let should_be_born = born(&cell);
+
+    // THEN
+    assert_eq!(should_be_born, false);
+}
+
+#[test]
+fn test_living_cell_stays_alive() {
+    // GIVEN
+    let cell = Cell::new(State::Live, Cell::create_neighbours(1));
+
+    // WHEN
+    let should_be_born = born(&cell);
+
+    // THEN
+    assert_eq!(should_be_born, false);
 }
