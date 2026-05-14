8446fd8fb0e4b93ba070e77f611088e561d5d995
jannik-reaZn <jannik.harm@gmail.com>
2026-05-14 14:35:58 +0000

    Implemented main

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
-use pixels::{Pixels, SurfaceTexture};
-use winit::event_loop::{ActiveEventLoop, EventLoop};
 fn main() {
-    game_of_life::presentation::render::run();
-    let event_loop = EventLoop::new();
-    // let window =
+    if let Err(error) = game_of_life::presentation::render::run() {
+        eprintln!("application error: {error}");
+    }
 }
