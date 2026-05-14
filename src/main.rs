c8900e45664e3928f45a21b996a82169d2efe56b
jannik-reaZn <jannik.harm@gmail.com>
2026-05-14 14:25:28 +0000

    Implemented inital resentation layer

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use pixels::{Pixels, SurfaceTexture};
+use winit::event_loop::{ActiveEventLoop, EventLoop};
 fn main() {
-    println!("Hello, world!");
+    game_of_life::presentation::render::run();
+    let event_loop = EventLoop::new();
+    // let window =
 }
---
+++ src/presentation/render.rs
@@ -0,0 +1,7 @@
+const WIDTH: u32 = 320;
+const HEIGHT: u32 = 240;
+const BOX_SIZE: i16 = 64;
+
+pub fn run() {
+    println!("Running the game...");
+}
