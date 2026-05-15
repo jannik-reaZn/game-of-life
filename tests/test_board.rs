030c2694fa22c8f1e219a653df81e8e932b732ec
jannik-reaZn <jannik.harm@gmail.com>
2026-05-15 13:49:47 +0000

    Implemented tests for board, moved from nested test setup to top level

---
+++ tests/test_board.rs
@@ -0,0 +1,48 @@
+use game_of_life::domain::board::Board;
+use game_of_life::domain::cell::Cell;
+use game_of_life::domain::state::State;
+use rstest::{fixture, rstest};
+
+#[fixture]
+fn sample_cells() -> Vec<Vec<Cell>> {
+    vec![
+        vec![Cell::new(State::Live), Cell::new(State::Dead)],
+        vec![Cell::new(State::Dead), Cell::new(State::Live)],
+    ]
+}
+
+#[rstest]
+fn test_board_initialization(sample_cells: Vec<Vec<Cell>>) {
+    // GIVEN
+    let cells = sample_cells.clone();
+
+    // WHEN
+    let board = Board::new(cells.clone());
+
+    // THEN
+    assert_eq!(board.cells(), &cells);
+}
+
+#[rstest]
+fn test_board_mutation(sample_cells: Vec<Vec<Cell>>) {
+    // GIVEN
+    let mut board = Board::new(sample_cells.clone());
+
+    // WHEN
+    board.cells_mut()[0][0].set_state(State::Dead);
+
+    // THEN
+    assert_eq!(board.cells()[0][0].state(), State::Dead);
+}
+
+#[rstest]
+fn test_get_living_neighbour(sample_cells: Vec<Vec<Cell>>) {
+    // GIVEN
+    let board = Board::new(sample_cells.clone());
+
+    // WHEN
+    let living_neighbours = board.get_living_neighbour();
+
+    // THEN
+    assert_eq!(*living_neighbours, 0); // Placeholder assertion
+}
